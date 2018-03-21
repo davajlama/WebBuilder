cc40c482babc9edee7b0ac7da33c072f0ee109d9
David Bittner <david.bittner@4every1.cz>
2018-03-21 10:03:10 +0100

    Reorganize structure

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,74 +1,10 @@
 <?php
 
-var_dump("david");
+require_once __DIR__ . '/../vendor/autoload.php';
 
-
-$builder = new WebHelp();
-
-$builder->src()->watch(function($src){
-	$this->src(__DIR__ . '/src')
-		->pipe(File::filter('~/\w+Bundle/src/resources/less/.*\.less~'))
-		->work(function($src){
-		
-			$this->retry();
-		});
-
-$bundlesLess = $this->src()->watch(function($src){
-	$src->pipe();
+$builder = new \Davajlama\WebBuilder\WebBuilder();
+$builder->source(__DIR__)->build(function(){
 
 });
-		
-});
 
-$builder->src(__DIR__ . '/src')
-		->pipe(Filter::regexp('~*Bundle/resources/less~'))
-		->worker(function($src){
-			$this->interval(5000);
-			$src->pipe();
-		});
-		
-
-interface Source
-{
-	public function check();
-}
-		
-
-// toto asi zat�m ne
-
-class Worker
-{
-	private $source;
-	
-	private $callback;
-	
-	private $interval;
-	
-	public function check();
-	
-	public function invoke();
-	
-	public function interval();
-}
-
-// watch
-foreach($workers as $worker) {
-	$worker->check() && $worker->invoke();
-}
-
-$source->isModified();
-
-
-
-$builder['less']['admin']->src()->watch();
-$builder['less']['front']->src()->watch();
-$builder->watch();
 $builder->build();
-
-// webhelp watch|build <task>
-// webhelp watch 
-// webhelp watch less
-// webhelp watch less:admin
-// webhelp build
-// webhelp build less
-// webhelp build less:admin
